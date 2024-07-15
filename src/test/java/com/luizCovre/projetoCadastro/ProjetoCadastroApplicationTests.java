131df7bc6b6e8d4cc25ac423c1001a9e6550e71b
Luiz Covre <luiz.1covre@gmail.com>
2024-07-15 10:31:02 -0300

    projeto criado

---
+++ src/test/java/com/luizCovre/projetoCadastro/ProjetoCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizCovre.projetoCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
